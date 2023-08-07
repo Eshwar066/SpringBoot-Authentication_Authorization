29394d5bf135940cba1a8ee1658c09b22ab3e85f
Eshwar Sai <121691549+Eshwar066@users.noreply.github.com>
2023-08-07 18:09:50 +0530

    Add files via upload

---
+++ SpringBoot-Authentication_Authorization-main/demo_SpringSecurity/src/main/java/com/example/demo_SpringSecurity/configration/SecurityConfiguration.java
@@ -0,0 +1,103 @@
+package com.example.demo_SpringSecurity.configration;
+
+import com.example.demo_SpringSecurity.utils.RSAKeyProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+import com.example.demo_SpringSecurity.utils.RSAKeyProperties;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Configuration
+public class SecurityConfiguration {
+	
+	private final RSAKeyProperties  keys;
+
+	 public SecurityConfiguration (RSAKeyProperties keys) {
+		 this.keys=keys;
+	 }
+
+//	@Bean                                                     //optional
+//	public PasswordEncoder passwordEncoder() {
+//		return new BCryptPasswordEncoder();
+//	}
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		String idForEncode = "bcrypt";
+		Map<String, PasswordEncoder> encoderMap = new HashMap<>();
+		encoderMap.put(idForEncode, new BCryptPasswordEncoder());
+		return new DelegatingPasswordEncoder(idForEncode, encoderMap);
+	}
+
+	@Bean 
+	public AuthenticationManager authManager(UserDetailsService detailsService) {
+		DaoAuthenticationProvider daoProvider=new DaoAuthenticationProvider();
+		daoProvider.setUserDetailsService(detailsService);
+//		daoProvider.setPasswordEncoder(passwordEncoder());                         //optional
+		return new ProviderManager(daoProvider);
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
+		return http
+				.csrf(csrf->csrf.disable())
+				.authorizeHttpRequests(auth->{
+					auth.requestMatchers(new AntPathRequestMatcher("/auth/**")).permitAll();
+					auth.anyRequest().authenticated();
+				})
+//				.httpBasic().and()
+				.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+				.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.build();
+	}
+	
+	@Bean
+	public JwtDecoder jwtDecoder() {
+		return NimbusJwtDecoder.withPublicKey(keys.getPublicKey()).build();
+	}
+
+	@Bean
+	public JwtEncoder jwtEncoder() {
+		JWK jwk = new RSAKey.Builder(keys.getPublicKey()).privateKey(keys.getPrivateKey()).build();
+		JWKSource<SecurityContext> jwks=new ImmutableJWKSet<>(new JWKSet(jwk));
+		return new NimbusJwtEncoder(jwks);
+	}
+
+//	@Bean
+//	public JwtAuthenticationConverter jwtAuthenticationConverter(){
+//		JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter=new JwtGrantedAuthoritiesConverter();
+//		jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("rles");
+//		jwtGrantedAuthoritiesConverter.setAuthorityPrefix("Role_");
+//		JwtAuthenticationConverter jwtConverter=new JwtAuthenticationConverter();
+//		jwtConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+//		return jwtConverter;
+//	}
+}
---
+++ SpringBoot-Authentication_Authorization-main/demo_SpringSecurity/src/main/java/com/example/demo_SpringSecurity/services/AuthenticationService.java
@@ -0,0 +1,83 @@
+package com.example.demo_SpringSecurity.services;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.demo_SpringSecurity.models.ApplicationUser;
+import com.example.demo_SpringSecurity.models.LoginResponseDTO;
+import com.example.demo_SpringSecurity.models.Role;
+import com.example.demo_SpringSecurity.repositary.RoleRepositary;
+import com.example.demo_SpringSecurity.repositary.UserRepositary;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class AuthenticationService {
+	
+	@Autowired
+	private UserRepositary userRepositary;
+	
+	@Autowired
+	private RoleRepositary roleRepositary;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private TokenService tokenService;
+	
+
+    
+	public ApplicationUser registerUser(String username, String password) {
+		String encodedPassword=passwordEncoder.encode(password);
+		Role userRole=roleRepositary.findByAuthority("USER").get();
+		Set<Role> authorities=new HashSet<>();
+		authorities.add(userRole);
+		
+		return userRepositary.save(new ApplicationUser(1,username,encodedPassword,authorities));
+	}
+	public LoginResponseDTO loginUser(String username,String password) {
+
+		try {
+
+			Authentication auth= authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(username,password)
+			);
+
+			String token=tokenService.generateJwt(auth);
+
+			return new LoginResponseDTO(userRepositary.findByUsername(username).get(),token);
+		}catch(AuthenticationException e) {
+
+			return new LoginResponseDTO(null,"");
+
+		}
+	}
+
+ }
+
+
+
+// private final DaoAuthenticationProvider authenticationProvider;
+//
+//    @Autowired
+//    public AuthenticationService(PasswordEncoder passwordEncoder) {
+//        this.authenticationProvider = new DaoAuthenticationProvider();
+//        this.authenticationProvider.setPasswordEncoder(passwordEncoder);
+//    }
+
+
